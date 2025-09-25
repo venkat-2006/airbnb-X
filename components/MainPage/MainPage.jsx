9e125f2e1194576b012a701a29bcbeb0ef3e93a8
VENKAT6912 <vibecoder18@gmail.com>
2025-09-25 19:46:01 +0530

    added file structure

---
+++ components/MainPage/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './MainPage.css'
+
+const MainPage = () => {
+  return (
+    <div>
+        2
+      
+    </div>
+  )
+}
+
+export default MainPage
---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+        1
+
+      
+    </div>
+  )
+}
+
+export default Navbar
