ff58ab83c42fc4197a214d3da33fd9e2624c0507
VENKAT6912 <vibecoder18@gmail.com>
2025-09-25 20:15:33 +0530

    positioned navbar text

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -4,7 +4,8 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div>
-        3
+        
+       
       
     </div>
   )
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../src/assets/logo.png'
 
 const Navbar = () => {
   return (
     <div>
-        1
+        <div className="top-nav">
+            <div className="logo">
+                <img src={logo} alt="logo" />
+            </div>
+            
+           
+            <span>
+                
+                 
+                <div className='homes'>Homes</div>
+                <div  className='experiences'>Experiences</div>
+                <div className='services'> Services</div>
+            </span>
+        </div>
 
       
     </div>
