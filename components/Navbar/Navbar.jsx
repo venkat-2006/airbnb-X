c709546c68bb6a2623636fbb70a0e89cf6dccabd
VENKAT6912 <vibecoder18@gmail.com>
2025-09-26 14:48:50 +0530

    added nav right

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../src/assets/logo.png'
+import internet_emoji_x from '../../src/assets/internet_emoji_x.jpg'
+import sandwich from '../../src/assets/sandwich.jpg'
+
 
 const Navbar = () => {
   return (
@@ -18,6 +21,12 @@ const Navbar = () => {
                 <div  className='experiences'>Experiences</div>
                 <div className='services'> Services</div>
             </span>
+            <div className="nav-right">
+              Become a host!
+              <img src={internet_emoji_x} alt="" className='internet' />
+              <img src={sandwich} alt="" className='sandwich' />
+
+            </div>
         </div>
 
       
