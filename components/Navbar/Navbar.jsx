3fe650e46ffa01e08d11d5abdb57d1edbc6d9975
VENKAT6912 <vibecoder18@gmail.com>
2025-09-26 15:36:49 +0530

    navbar without animation/emojis

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -3,33 +3,61 @@ import './Navbar.css'
 import logo from '../../src/assets/logo.png'
 import internet_emoji_x from '../../src/assets/internet_emoji_x.jpg'
 import sandwich from '../../src/assets/sandwich.jpg'
+import search_emoji from '../../src/assets/search_emoji.jpg'
 
 
 const Navbar = () => {
   return (
     <div>
-        <div className="top-nav">
-            <div className="logo">
-                <img src={logo} alt="logo" />
-            </div>
-            
-           
-            <span>
-                
-                 
-                <div className='homes'>Homes</div>
-                <div  className='experiences'>Experiences</div>
-                <div className='services'> Services</div>
-            </span>
-            <div className="nav-right">
-              Become a host!
-              <img src={internet_emoji_x} alt="" className='internet' />
-              <img src={sandwich} alt="" className='sandwich' />
-
-            </div>
+      <div className="top-nav">
+        <div className="logo">
+          <img src={logo} alt="logo" />
         </div>
 
-      
+
+        <span>
+
+
+          <div className='homes'>Homes</div>
+          <div className='experiences'>Experiences</div>
+          <div className='services'> Services</div>
+        </span>
+        <div className="nav-right">
+          Become a host!
+          <img src={internet_emoji_x} alt="" className='internet' />
+          <img src={sandwich} alt="" className='sandwich' />
+
+        </div>
+
+      </div>
+    <div className="nav-bottom">
+  <div className="search-bar">
+    <div className="search-items-container">
+      <div className="search-item">
+        <h2>Where</h2>
+        <p>Search destinations</p>
+      </div>
+      <div className="divider"></div>
+      <div className="search-item">
+        <h2>When</h2>
+        <p>Add dates</p>
+      </div>
+      <div className="divider"></div>
+      <div className="search-item">
+        <h2>Who</h2>
+        <p>Add guests</p>
+      </div>
+    </div>
+    <div className="search-button">
+      <img src={search_emoji} alt="Search" />
+    </div>
+  </div>
+</div>
+
+
+
+
+
     </div>
   )
 }
